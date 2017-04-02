36ec5afd6e6aad25e9ca0b20ef8aeb58a070b538
goliatone <burgosemi@gmail.com>
2017-04-02 19:48:14 -0400

    add express example

---
+++ example/express/routes/users.js
@@ -0,0 +1,23 @@
+var express = require('express');
+var router = express.Router();
+
+var Menu = require('/Users/goliatone/Development/NODEJS/gmenu');
+
+let userMenu = Menu.get('admin').addNode('Users');
+userMenu.addNode('Pets');
+userMenu.addNode('Profiles');
+
+/* GET users listing. */
+router.get('/', function(req, res) {
+  res.render('index', { title: 'Users' });
+});
+
+router.get('/pets', function(req, res) {
+  res.render('index', { title: 'Pets' });
+});
+
+router.get('/profiles', function(req, res) {
+  res.render('index', { title: 'Profiles' });
+});
+
+module.exports = router;
