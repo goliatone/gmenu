613666bced5b70f76d67d38f576f15a22cefaa81
goliatone <burgosemi@gmail.com>
2017-04-08 20:36:58 -0400

    Add pets route to example

---
+++ example/express/routes/pets.js
@@ -0,0 +1,35 @@
+'use strict';
+
+var express = require('express');
+var router = express.Router();
+
+var Menu = require('gmenu');
+
+/*
+ * Create Pets menu, we can access it using:
+ */
+let petsMenu = Menu.get('admin').addNode('Pets');
+petsMenu.addNode('Profiles');
+petsMenu.addNode('Owners');
+
+let subMenu = petsMenu.addNode('Types');
+subMenu.addNode('Cats');
+subMenu.addNode('Dogs');
+
+/* GET users listing. */
+router.get('/types/:type?', function(req, res){
+    res.render('index', { title: 'Types' });
+});
+
+router.get('/owners', function(req, res){
+    res.render('index', { title: 'Owners' });
+});
+router.get('/profiles', function(req, res){
+    res.render('index', { title: 'Profiles' });
+});
+router.get('/', function(req, res){
+    res.render('index', { title: 'Pets' });
+});
+
+
+module.exports = router;
